55c5d15612b731def6dfcc433cda5fa7fe6bd505
Angel decode <miguelangelpacompia@gmail.com>
2022-05-14 21:17:57 -0500

    header complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+
 import "./header.css"
+
+import ME from "../../assets/me.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Angel Choque</h1>
+        <h5 className='text-light'>Full Stack Developer</h5>
+
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="Angel Choque" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
